38e7de73b652a4ca20d6dd567a1684440a7048a0
Jeroen Gilis <gilis@chalmers.se>
2024-10-17 17:13:49 +0200

    avoid intermediate files

--- code/construct_drafts.m
+++ code/construct_drafts.m
@@ -1,37 +1,38 @@
-% replace original gene IDs with orthology cluster labels
+% replace original gene IDs with orthology cluster labels to construct
+% Aspergillus_draft for each species
 
-% Fumigatus
+% Load orthology info from BPGA
+BPGA=readtable('../data/genome/BPGA2ortho_GEM_custom.csv', 'Delimiter', ';');
+
+%% Fumigatus
 model=importModel("../model/templates/pan_afm.xml",false,false,true)
-id2clust=readtable("../data/genome/fumi_temp.csv")
-id2clust = table2cell(id2clust)
+translationTable=id2clust(BPGA,'Afumigatus') 
 
-[draftModel,removedRxns]=getModelFromOrthology(model,id2clust)
+[draftModel,removedRxns]=getModelFromOrthology(model,translationTable)
 exportModel(draftModel,"../model/Afu_draft.xml")
 exportToExcelFormat(draftModel,"../model/Afu_draft.xlsx")
+%%
 
-% Oryzae
+%% Oryzae
 model=importModel("../model/templates/iWV1314.xml",false,false,true)
 %%getModelFromOrthology fails w/o annotation: add placeholder
 model.annotation=struct("placeholder","placeholder") 
-id2clust=readtable("../data/genome/ory_temp.csv")
-id2clust=table2cell(id2clust)
+translationTable=id2clust(BPGA,'Aoryzae')
 
-[draftModel,removedRxns]=getModelFromOrthology(model,id2clust)
+[draftModel,removedRxns]=getModelFromOrthology(model,translationTable)
 
 exportModel(draftModel,"../model/Aory_draft.xml")
 exportToExcelFormat(draftModel,"../model/Aory_draft.xlsx")
+%%
 
-% Niger
+%% Niger
 model=importModel("../model/templates/iJB1325.xml",false,false,true)
 %%getModelFromOrthology fails w/o annotation: add placeholder
 model.annotation=struct("placeholder","placeholder")
+translationTable=id2clust(BPGA,'Aniger') 
 
-opts = detectImportOptions("../data/genome/nig_temp.csv");
-opts = setvartype(opts,"Aniger",'char');  %or 'char' if you prefer
-id2clust=readtable("../data/genome/nig_temp.csv",opts)
-id2clust=table2cell(id2clust)
-
-[draftModel,removedRxns]=getModelFromOrthology(model,id2clust)
+[draftModel,removedRxns]=getModelFromOrthology(model,translationTable)
 
 exportModel(draftModel,"../model/Anig_draft.xml")
 exportToExcelFormat(draftModel,"../model/Anig_draft.xlsx")
+%%
