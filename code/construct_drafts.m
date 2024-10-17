2ae72458c9a6c10dfdf38626359d842d6ea2e445
Jeroen Gilis <gilis@chalmers.se>
2024-10-17 15:32:30 +0200

    oryzae draft

---
+++ code/construct_drafts.m
@@ -0,0 +1,34 @@
+% replace original gene IDs with orthology cluster labels
+
+% Fumigatus
+model=importModel("../model/templates/pan_afm.xml",false,false,true)
+id2clust=readtable("../data/genome/fumi_temp.csv")
+id2clust = table2cell(id2clust)
+
+[draftModel,removedRxns]=getModelFromOrthology(model,id2clust)
+exportModel(draftModel,"../model/Afu_draft.xml")
+exportToExcelFormat(draftModel,"../model/Afu_draft.xlsx")
+
+% Oryzae
+model=importModel("../model/templates/iWV1314.xml",false,false,true)
+%%getModelFromOrthology fails w/o annotation: add placeholder
+model.annotation=struct("placeholder","placeholder") 
+id2clust=readtable("../data/genome/ory_temp.csv")
+id2clust=table2cell(id2clust)
+
+[draftModel,removedRxns]=getModelFromOrthology(model,id2clust)
+
+exportModel(draftModel,"../model/Aory_draft.xml")
+exportToExcelFormat(draftModel,"../model/Aory_draft.xlsx")
+
+% Niger
+model=importModel("../model/templates/iJB1325.xml",false,false,true)
+%%getModelFromOrthology fails w/o annotation: add placeholder
+model.annotation=struct("placeholder","placeholder") 
+id2clust=readtable("../data/genome/nig_temp.csv")
+id2clust=table2cell(id2clust)
+
+[draftModel,removedRxns]=getModelFromOrthology(model,id2clust)
+
+exportModel(draftModel,"../model/Aory_draft.xml")
+exportToExcelFormat(draftModel,"../model/Aory_draft.xlsx")
--- code/xml_to_xlsx.m
+++ code/xml_to_xlsx.m
@@ -4,20 +4,20 @@
 % xml obtained from:
 % https://github.com/opencobra/m_model_collection/blob/master/sbml3/iWV1314.xml
 
-model=importModel("../models/templates/iWV1314.xml",false,false,true)
+model=importModel("../model/templates/iWV1314.xml",false,false,true)
 % obtained without RAVEN/libSBML errors
 
-exportToExcelFormat(model,"../models/templates/iWV1314.xlsx")
+exportToExcelFormat(model,"../model/templates/iWV1314.xlsx")
 
 %%%%%%%%%%%%%% Aspergillus fumigatus %%%%%%%%%%%%%%
 % Inspecting the Aspergillus fumigatus GEM from Mirhakkak et al. (2023)
 % xml obtained from:
 % https://www.ebi.ac.uk/biomodels/MODEL2211100001#Files (the pan GEM)
 
-model=importModel("../models/templates/pan_afm.xml",false,false,true)
+model=importModel("../model/templates/pan_afm.xml",false,false,true)
 % obtained without RAVEN/libSBML errors
 
-exportToExcelFormat(model,"../models/templates/pan_afm.xlsx")
+exportToExcelFormat(model,"../model/templates/pan_afm.xlsx")
 
 %%%%%%%%%%%%%% Aspergillus niger %%%%%%%%%%%%%%
 % Inspecting the Aspergillus niger GEM from Brandl et al. (2018)
@@ -25,7 +25,7 @@ exportToExcelFormat(model,"../models/templates/pan_afm.xlsx")
 % https://fungalbiolbiotech.biomedcentral.com/articles/10.1186/s40694-018-0060-7
 
 %%%%%%%%%%%% import
-model=importModel("../models/templates/iJB1325.xml",false,false,true)
+model=importModel("../model/templates/iJB1325.xml",false,false,true)
 
 % The model contains 3741 errors.
 % Assess using validator webtool: https://sbml.bioquant.uni-heidelberg.de/validator_servlet/index.jsp. 
@@ -49,4 +49,4 @@ model=importModel("../models/templates/iJB1325.xml",false,false,true)
 
 %%%%%%%%%%%% export
 %function exportToExcelFormat(model,fileName,sortIds)
-exportToExcelFormat(model,"../models/templates/iJB1325.xlsx")
+exportToExcelFormat(model,"../model/templates/iJB1325.xlsx")
